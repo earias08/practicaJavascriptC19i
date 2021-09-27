1d3d0efa813955ab323de10b52aef34301f87434
Emilse <emilsearias@gmail.com>
2021-09-27 18:35:56 -0300

    se agrego el archivo de bucles

---
+++ js/3-bucles.js
@@ -0,0 +1,11 @@
+document.write('Hola mundo linea 1');
+document.write('<br>Hola mundo linea 2');
+document.write('<br>Hola mundo linea 3');
+document.write('<br>Hola mundo linea 4');
+document.write('<br>Hola mundo linea 5');
+
+// while
+
+//do-while
+
+//for
