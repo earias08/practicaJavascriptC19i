c8104baaea1b6e2d3bb558fa955da4673fe0b6e7
Emilse <emilsearias@gmail.com>
2021-09-29 18:39:42 -0300

    se agrego el ejercicio 9 del tp 1 y las estructuras de los bucles

--- js/3-bucles.js
+++ js/3-bucles.js
@@ -1,11 +1,45 @@
-document.write('Hola mundo linea 1');
-document.write('<br>Hola mundo linea 2');
-document.write('<br>Hola mundo linea 3');
-document.write('<br>Hola mundo linea 4');
-document.write('<br>Hola mundo linea 5');
+// document.write('Hola mundo linea 1');
+// document.write('<br>Hola mundo linea 2');
+// document.write('<br>Hola mundo linea 3');
+// document.write('<br>Hola mundo linea 4');
+// document.write('<br>Hola mundo linea 5');
+// document.write('<br>Hola mundo linea 6');
+// document.write('<br>Hola mundo linea 7');
 
-// while
+// while ->mientras
+/* while(condicion logica){ 
+    todas las lineas de codigo que me interensan
+    cambie la condicion logica
+}
+*/
+// let contador = 1;
 
-//do-while
+// while( contador <= 50){
+//     document.write('Hola mundo linea '+contador+'<br>');
+//     // contador++; contador-- 
+//     contador = contador + 1;
+// }
 
-//for
+//do-while hacer ... mientras
+/*
+    do{
+         todas las lineas de codigo que me interensan
+        cambie la condicion logica
+    }while(condicion logica)
+*/
+
+// let contar = 40;
+
+// do{
+//     document.write('Hola mundo linea '+contar+'<br>');
+//     contar--; //contar = contar - 1;
+// }while(contar >= 1)
+
+
+/*for(declarar un variable; condicion logica; incrementar o decrementar la variable){
+    todas las lineas de codigo que me interensan
+}*/
+
+for(let indice = 1; indice <= 50 ; indice = indice + 1 ){
+    document.write('Hola mundo linea '+indice+'<br>');
+}
---
+++ js/ejercicios/ej9-tp1.js
@@ -0,0 +1,14 @@
+let frase = prompt('Ingrese una frase');
+
+console.log(frase);
+console.log(frase.length);
+console.log(frase.charAt(0));
+
+// toUppercase , ToLowercase
+
+for(let posicion = 0; posicion < frase.length; posicion++ ){
+    if(frase.charAt(posicion) =='a' || frase.charAt(posicion) =='e' || frase.charAt(posicion) =='i' || frase.charAt(posicion) =='o' || frase.charAt(posicion) =='u'){
+        document.write(frase.charAt(posicion));
+    }
+}
+
