3cb6b33ce753a36d72dfc555c9677fcb1f343219
Emilse <emilsearias@gmail.com>
2021-10-20 17:32:32 -0300

    agregamos logica al archivo de objetos

--- js/7-objetos.js
+++ js/7-objetos.js
@@ -14,17 +14,26 @@ let usuarioPablo = {
     localidad: 't4000',
     telefono: 3814534634,
     dni: 35666777,
-    estado: true
+    estado: true,
     // metodos
+    login: function (){
+        //todo el codigo que quiero que este metodo
+        console.log('Estamos logueados');
+    },
+    logout: () =>{
+        // todo el codigo que quiero que haga este metodo de logout
+        console.log('Cerramos sesion');
+    }
 }
 
 // mostrar un objeto
 console.log(usuarioPablo);
 
+
 // mostrar cada propiedad del objeto - forma 1
 document.write('Email: '+ usuarioPablo.email);
 document.write('<br>localidad: '+ usuarioPablo.localidad);
-
+document.write('<br>DNI: '+ usuarioPablo.dni);
 // mostrar cada propiedad del objeto - forma 2
 document.write('<br>Telefono: '+ usuarioPablo['telefono'] );
 
@@ -32,3 +41,18 @@ document.write('<br>Telefono: '+ usuarioPablo['telefono'] );
 usuarioPablo.nivel = 1;
 document.write('<br>Nivel de usuario: '+ usuarioPablo.nivel);
 
+// modificar una propiedad del objeto
+usuarioPablo.dni = 37666343;
+
+// queremos mostrar todas las propiedades (claves) de un objeto
+let claves = Object.keys(usuarioPablo);
+console.log(claves);
+
+for(let posicion=0; posicion < claves.length ; posicion++){
+    document.write('<br>'+claves[posicion]+': '+usuarioPablo[claves[posicion]]);
+}
+
+// usar los metodos de un objeto
+usuarioPablo.login();
+
+usuarioPablo.logout();
